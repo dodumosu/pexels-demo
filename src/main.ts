889b420750bc9065834f5435a725b10bb3f184a4
Odumosu Dipo <ironkyo@gmail.com>
2022-03-28 18:29:37 +0100

    feat: enable config and setup client

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
-import './style.css'
+import './config';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const PEXELS_API_KEY = process.env.PEXELS_API_KEY;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+interface Photo {
+  id: number;
+  width: number;
+  height: number;
+  url: string;
+  photographer: string;
+  photographer_url: string;
+  photographer_id: string;
+  avg_color: string;
+  src: {
+    original: string;
+    large2x: string;
+    large: string;
+    medium: string;
+    small: string;
+    portrait: string;
+    landscape: string;
+    tiny: string;
+  };
+}
+
+interface PhotoSearchAPIResult {
+  total_results: number;
+  page: number;
+  per_page: number;
+  photos: Photo[];
+  next_page: string;
+}
+
+async function fetchImagesFromAPI(searchTerm: string, perPage: number): Promise<PhotoSearchAPIResult> {
+  const result = await fetch(
+    `https://api.pexels.com/v1/search?query=${searchTerm}&per_page=${perPage}`,
+    {
+      headers: {
+        Authorization: PEXELS_API_KEY
+      },
+    }
+  );
+
+  const json = (await result.json()) as PhotoSearchAPIResult;
+  return json;
+}
