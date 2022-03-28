6b6403c96714a3e1483d43827cee13c3f5aebd2e
Odumosu Dipo <ironkyo@gmail.com>
2022-03-28 21:28:12 +0100

    feat: add linting

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {html, render} from 'lit-html';
+import {html, nothing, render} from 'lit-html';
 
 const PEXELS_API_KEY = import.meta.env.VITE_PEXELS_API_KEY;
 
@@ -45,20 +45,41 @@ async function fetchImagesFromAPI(searchTerm: string, perPage: number): Promise<
   return json;
 }
 
-fetchImagesFromAPI('dogs', 5).then((data) => {
+async function onFormSubmit(event: SubmitEvent) {
+  event.preventDefault();
+  if (!event.target)
+    return;
+
+  const formData = new FormData(event.target as HTMLFormElement);
+  const query = formData.get('search-query');
+  if (query && typeof query === 'string') {
+    const results = await fetchImagesFromAPI(query, 10);
+    renderApp(results);
+  }
+}
+
+function renderApp(results: PhotoSearchAPIResult | null): void {
+  const div = document.getElementById('app');
+  if (!div)
+    throw new Error('could not find app div');
+
   const htmlToRender = html`
-    <h1>Results for "dogs"</h1>
+    <h1>Amazing Photo App</h1>
+
+    <form id="search" @submit=${onFormSubmit}>
+      <input type="text" name="search-query" placeholder="dogs" />
+      <input type="submit" value="Search" />
+    </form>
     <ul>
-      ${data.photos.map((photo) => {
-        return html`<li><img src=${photo.src.small} /></li>`;
-      })}
+      ${results
+        ? results.photos.map(photo => {
+          return html`<li><img src=${photo.src.small} /></li>`;
+        })
+        : nothing
+      }
     </ul>
   `;
-
-  const div = document.getElementById('app');
-  if (!div) {
-    throw new Error('could not find app div');
-  }
-
   render(htmlToRender, div);
-});
+}
+
+renderApp(null);
